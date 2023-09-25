752d433416bbe6a79818de9e1d2ba0c252db31ca
cather111 <2829397175@qq.com>
2023-09-25 19:51:02 +0800

    ver3_ex

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 chcp 65001
 
-for /l %%i in (1,1,10) do (
-    python main.py --task "PHA_5tenant_3community_19house" >> test.log
+for /l %%i in (1,1,3) do (
+    python main.py --task "PHA_5tenant_3community_19house_ver3_nofilter" >> test.log
     echo This is the %%i loop 
 )
